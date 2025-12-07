a584e4a57c0d65c9a6cfefafe1ba4fb15f8f95b3
Biloliddin-Tursunov <tursunovbiloliddin2006@gmail.com>
2025-12-08 01:27:08 +0500

    add new logo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import { Scale, Search, X } from "lucide-react";
 import { supabase } from "../supabaseClient";
 import styles from "../styles/header.module.css";
+import Logo from "../assets/logo.png";
 
 const Header = ({ onTermSelect }) => {
     const [query, setQuery] = useState("");
@@ -73,7 +74,7 @@ const Header = ({ onTermSelect }) => {
                 */}
 
                 <div className={styles.iconWrapper}>
-                    <Scale size={64} className={styles.icon} />
+                    <img src={Logo} className={styles.icon} />
                 </div>
 
                 <h1 className={styles.title}>Yuridik Lug'at va Atamalar</h1>
